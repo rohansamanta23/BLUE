b3af1e2e9e66203bd8f72237b4fbdd815e80af44
rohan samanta <rohansamanta45@gmail.com>
2026-04-06 16:21:19 +0530

    prop2 error fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Services />   
       <OngoingProjects />
       <PastProjects />  
-      <Careers />    
+      {/* <Careers />     */}
       <Contact />    
       <Footer />     
 
